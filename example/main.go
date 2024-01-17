05ae0e32bc58af434a4cd4355f3a14ad628d5596
TangGaoDou <opensh@msn.com>
2024-01-17 16:24:19 +0800

    format example

--- example/main.go
+++ example/main.go
@@ -17,7 +17,7 @@ func main() {
 			panic(err)
 		}
 		for _, v := range s {
-			fmt.Fprintf(writer, "Device:%s\nrrqm/s:%s\nwrqm/s:%s\nr/s:%s\nw/s:%s\nrMB/s:%s\nwMB/s:%s\navgrq-sz:%s\navgqu-sz:%s\nawait:%s\nr_await:%s\nw_await:%s\n%%util:%s", v.Device, v.RRQM, v.WRQM, v.R, v.W, v.RMB, v.WMB, v.AvgrqSz, v.AvgquSz, v.Await, v.RWait, v.WWait, v.Util)
+			fmt.Fprintf(writer, "device:           rrqm/s  wrqm/s      r/s      w/s    rMB/s    wMB/s avgrq-sz avgqu-sz     await   r_await   w_await   %%util\n%s        %s  %s      %s      %s    %s    %s %s %s     %s   %s   %s   %s\n", v.Device, v.RRQM, v.WRQM, v.R, v.W, v.RMB, v.WMB, v.AvgrqSz, v.AvgquSz, v.Await, v.RWait, v.WWait, v.Util)
 		}
 	}
 
