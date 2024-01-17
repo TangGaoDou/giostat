c9085e6f46a4eded0106716e1b364c0791c663f5
TangGaoDou <opensh@msn.com>
2024-01-17 16:31:00 +0800

    update format

--- example/main.go
+++ example/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	// fmt.Println("device:           rrqm/s  wrqm/s      r/s      w/s    rMB/s    wMB/s avgrq-sz avgqu-sz     await   r_await   w_await   %%util")
+	fmt.Println("device:           rrqm/s  wrqm/s      r/s      w/s    rMB/s    wMB/s avgrq-sz avgqu-sz     await   r_await   w_await   %%util")
 	writer := uilive.New()
 	writer.Start()
 	for {
@@ -17,7 +17,7 @@ func main() {
 			panic(err)
 		}
 		for _, v := range s {
-			fmt.Fprintf(writer, "device:           rrqm/s  wrqm/s      r/s      w/s    rMB/s    wMB/s avgrq-sz avgqu-sz     await   r_await   w_await   %%util\n%s        %s  %s      %s      %s    %s    %s %s %s     %s   %s   %s   %s\n", v.Device, v.RRQM, v.WRQM, v.R, v.W, v.RMB, v.WMB, v.AvgrqSz, v.AvgquSz, v.Await, v.RWait, v.WWait, v.Util)
+			fmt.Fprintf(writer, "%s\t\t%s%s%s%s%s%s%s%s%s%s%s%s\n", v.Device, v.RRQM, v.WRQM, v.R, v.W, v.RMB, v.WMB, v.AvgrqSz, v.AvgquSz, v.Await, v.RWait, v.WWait, v.Util)
 		}
 	}
 
